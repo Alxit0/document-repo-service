67942814b92ca187ccbeb2388d77ca8a5ab6d9a0
APedrosa13 <45878777+AntonioMSPMoreira13@users.noreply.github.com>
2024-11-13 15:56:49 +0000

    Test 5 a funcionar

--- src/tests/test.sh
+++ src/tests/test.sh
@@ -12,10 +12,17 @@ PRIVATE_KEY_FILE="private_key.pem"
 SESSION_FILE="session_token.json"
 DOCUMENT_PATH="$HOME/Documents/sample.txt"
 DOCUMENT_NAME="Document1"
-ORGANIZATION="NewOrg"
-USERNAME="orguser"
-USER_NAME="Org User"
-EMAIL="orguser@example.com"
+ORGANIZATION="Org1"
+USERNAME="User"
+USER_NAME="User Name"
+EMAIL="User@example.com"
+
+# Check if the sample document exists, create if not
+if [ ! -f "$DOCUMENT_PATH" ]; then
+    echo "Creating a new sample document at $DOCUMENT_PATH."
+    echo "This is a sample document for testing purposes." > "$DOCUMENT_PATH"
+fi
+
 
 # Change to the 'cli' directory
 cd src/cli
@@ -24,18 +31,27 @@ echo "Test: Step 1 - Generate RSA Key Pair"
 # Step 1: Generate RSA Key Pair
 ./rep_subject_credentials "$PASSWORD" "$CREDENTIALS_FILE"
 
+echo " "
 # Step 2: Create New Organization
 echo "Test: Step 2 - Create New Organization"
 # Create the organization
 ./rep_create_org "$ORGANIZATION" "$USERNAME" "$USER_NAME" "$EMAIL" "$PUBLIC_KEY_FILE"
 
+echo " "
 # Step 3: List All Organizations
 echo "Test: Step 3 - List All Organizations"
 # List organizations
 ./rep_list_orgs
 
-# Step 4: Add Document to Repository
-echo "Test: Step 4 - Add Document to Repository"
+echo " "
+# Step 4: Creat a session
+echo "Test: Step 4 - Creat a session"
+# Add the document to the repository
+./rep_create_session "$ORGANIZATION" "$USERNAME" "$PASSWORD" "$CREDENTIALS_FILE" "$SESSION_FILE"
+
+echo " "
+# Step 5: Add Document to Repository
+echo "Test: Step 5 - Add Document to Repository"
 # Add the document to the repository
 ./rep_add_doc "$SESSION_FILE" "$DOCUMENT_NAME" "$DOCUMENT_PATH"
 
