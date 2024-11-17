3320c1010305724d6a29aafcdb83a5ba6d39ae87
APedrosa13 <45878777+AntonioMSPMoreira13@users.noreply.github.com>
2024-11-17 16:13:32 +0000

    test_all.sh added

--- src/tests/Delete_files.sh
+++ src/tests/Delete_files.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 #delete database file
-rm delivery?/src/api/database.db
+rm src/api/database.db
 
 #delete org credentials
-rm delivery?/src/cli/*_cred.json
+rm src/cli/*_cred.json
 
 #delete org sessions
-rm delivery?/src/cli/*_session.json
+rm src/cli/*_session.json
 
 #delete sample files
 rm $HOME/Documents/*_?.txt
 
 #delete sample files
 rm $HOME/Downloads/*_?_copy.txt
+
+#delete ./sio dictory in $HOME
+rm -rf $HOME/.sio
